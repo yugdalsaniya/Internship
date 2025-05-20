6e5b86bd15c38d1b769f0d0827fcfbb68289223b
yugdalsaniya <yugpatel231078@gmail.com>
2025-05-20 12:28:34 +0530

    internship page start

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import HomePage from "../Pages/HomePage"; // Import your homepage component
+import Internship from "../Pages/InternshipPage"; // Import your internship page component
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
+        
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} /> {/* Add this for the root path */}
+          <Route path="/internship" element={<Internship  />} /> {/* Add this for the internship page */}
+          {/* Add more routes as needed */}
         </Route>
       </Routes>
     </Router>
