25f7c840a1292080b295e3d5cc85624b28e4c295
yugdalsaniya <yugpatel231078@gmail.com>
2025-05-26 18:35:31 +0530

    font change to kanit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kanit: ['Kanit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
