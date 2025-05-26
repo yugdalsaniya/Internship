ef0b55516a6e6dbfc96e4e1a93cb9435dcb667e0
nivchangela <nivchangela@gmail.com>
2025-05-26 13:04:27 +0530

    InternshipDetail Page

--- src/Pages/InternshipDetailPage.jsx
+++ src/Pages/InternshipDetailPage.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import bgImage from "../assets/Hero/banner.jpg"; // Adjust path based on your project structure
+import Hero from "../assets/Hero/banner.jpg"; // Adjust path based on your project structure
 
 const InternshipDetailPage = () => {
   return (
     <div
       className="w-full h-[300px] bg-cover bg-center relative flex items-center justify-center text-center"
-      style={{ backgroundImage: `url(${bgImage})` }}
+      style={{
+        backgroundImage: `linear-gradient(to right, rgba(249, 220, 223, 0.8), rgba(181, 217, 211, 0.8)), url(${Hero})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
     >
-      <div className="absolute inset-0 bg-gradient-to-r from-pink-200 via-white to-teal-200 opacity-80"></div>
-
-      <div className="relative flex flex-col items-center text-center max-w-7xl mx-auto">
+      <div className="relative flex flex-col items-center text-center max-w-7xl mx-auto z-10">
         <h1 className="text-3xl md:text-4xl font-bold text-[#050748] mb-3">
-         <b>Internships Details</b>
+          Internships Details
         </h1>
         <p className="text-base md:text-lg text-[#45457D] mb-6 max-w-3xl">
           "Empower Your Future: Unleash Limitless Career Possibilities!"
@@ -22,4 +24,3 @@ const InternshipDetailPage = () => {
 };
 
 export default InternshipDetailPage;
-    
